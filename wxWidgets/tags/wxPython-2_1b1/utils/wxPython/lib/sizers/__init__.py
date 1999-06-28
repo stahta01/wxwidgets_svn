4cdc0f5afded66614f59f1f1ef48784fa796b69e
nobody <nobody@c3d73ce0-8a6f-49c7-b76d-6d57e0e08775>
1999-06-28 04:08:58 +0000

    This commit was manufactured by cvs2svn to create tag 'wxPython-2_1b1'.

---
+++ wxWidgets/tags/wxPython-2_1b1/utils/wxPython/lib/sizers/__init__.py
@@ -0,0 +1,18 @@
+#----------------------------------------------------------------------------
+# Name:         __init__.py
+# Purpose:      The presence of this file turns this directory into a
+#               Python package.
+#
+# Author:       Robin Dunn
+#
+# Created:      18-May-1999
+# RCS-ID:       $Id$
+# Copyright:    (c) 1998 by Total Control Software
+# Licence:      wxWindows license
+#----------------------------------------------------------------------------
+
+from sizer   import *
+from box     import *
+from border  import *
+
+#----------------------------------------------------------------------------
