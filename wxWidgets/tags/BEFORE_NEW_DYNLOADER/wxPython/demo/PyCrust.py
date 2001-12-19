a87632a958a2eb0228305015dd60bd89379bd35b
nobody <nobody@c3d73ce0-8a6f-49c7-b76d-6d57e0e08775>
2001-12-19 01:56:38 +0000

    This commit was manufactured by cvs2svn to create tag 'BEFORE_NEW_DYNLOADER'.

---
+++ wxWidgets/tags/BEFORE_NEW_DYNLOADER/wxPython/demo/PyCrust.py
@@ -0,0 +1,16 @@
+
+
+from wxPython.lib.PyCrust import shell, version
+
+
+#----------------------------------------------------------------------
+
+intro = 'Welcome To PyCrust %s - The Flakiest Python Shell' % version.VERSION
+
+def runTest(frame, nb, log):
+    win = shell.Shell(nb, -1, introText=intro)
+    return win
+
+#----------------------------------------------------------------------
+
+overview = shell.__doc__
