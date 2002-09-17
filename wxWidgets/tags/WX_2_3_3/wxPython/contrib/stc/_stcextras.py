56d0d648de482cd7e9b03211d03812e2a6e11d86
nobody <nobody@c3d73ce0-8a6f-49c7-b76d-6d57e0e08775>
2002-09-17 23:24:39 +0000

    This commit was manufactured by cvs2svn to create tag 'WX_2_3_3'.

---
+++ wxWidgets/tags/WX_2_3_3/wxPython/contrib/stc/_stcextras.py
@@ -0,0 +1,7 @@
+# Stuff these names into the wx namespace so wxPyConstructObject can find them
+
+wx.wxStyledTextEventPtr       = wxStyledTextEventPtr
+wx.wxStyledTextCtrlPtr        = wxStyledTextCtrlPtr
+
+
+wxSTC_CARET_CENTER = wxSTC_CARET_STRICT
