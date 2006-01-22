5f61e03761f00e4682c69ce82303341dc50d94e1
nobody <nobody@c3d73ce0-8a6f-49c7-b76d-6d57e0e08775>
2006-01-22 13:45:34 +0000

    This commit was manufactured by cvs2svn to create tag 'BEFORE_GTK1_GTK_2_SPLIT'.

---
+++ wxWidgets/tags/BEFORE_GTK1_GTK_2_SPLIT/src/mac/carbon/gdiobj.cpp
@@ -0,0 +1,18 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        gdiobj.cpp
+// Purpose:     wxGDIObject class
+// Author:      Stefan Csomor
+// Modified by:
+// Created:     1998-01-01
+// RCS-ID:      $Id$
+// Copyright:   (c) Stefan Csomor
+// Licence:       wxWindows licence
+/////////////////////////////////////////////////////////////////////////////
+
+#include "wx/wxprec.h"
+
+#include "wx/gdiobj.h"
+
+IMPLEMENT_DYNAMIC_CLASS(wxGDIObject, wxObject)
+
+// TODO: Nothing to do, unless you want to.
