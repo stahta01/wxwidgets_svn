5a86bf31aed21318b82d283d0cf6730c02e9336d
RD <RD@c3d73ce0-8a6f-49c7-b76d-6d57e0e08775>
2009-03-27 21:50:05 +0000

    Detabified. Use PATH to check availability of name.exe instead of trying to run it with /help.  (This could be used in the other builders too...)

--- wxWidgets/tools/build/builder.py
+++ wxWidgets/tools/build/builder.py
@@ -7,46 +7,48 @@ import time
 class BuildError(Exception):
     def __init__(self, value):
         self.value = value
-        
+
     def __repr__(self):
         return repr(self.value)
 
-def runInDir(command, dir=None):
+def runInDir(command, dir=None, verbose=True):
     if dir:
         olddir = os.getcwd()
         os.chdir(dir)
 
     commandStr = string.join(command, " ")
+    if verbose:
+        print commandStr
     result = os.system(commandStr)
 
     if dir:
         os.chdir(olddir)
-        
+
     return result
 
 class Builder:
     """
     Base class exposing the Builder interface.
     """
-    
+
     def __init__(self, formatName="", commandName="", programDir=None):
         """
         formatName = human readable name for project format (should correspond with Bakefile names)
         commandName = name of command line program used to invoke builder
         programDir = directory program is located in, if not on the path
         """
-        
+
         self.dir = dir
         self.name = commandName
         self.formatName = formatName
         self.programDir = programDir
         self.doSetup()
-        
+
     def doSetup(self):
         """
         Do anything special needed to configure the environment to build with this builder.
         """
-        
+
         pass
 
     def isAvailable(self):
@@ -65,19 +67,19 @@ class Builder:
                 if result == 0:
                     return True
                 dirs = os.environ["PATH"].split(":")
-                
+
                 for dir in dirs:
                     if os.path.isfile(os.path.join(dir, self.name)):
                         return True  
-            
+
             else:
                 result = os.system("which %s" % self.name)
-        
+
                 if result == 0:
                     return True
-            
+
         return False
-        
+
     def getProgramPath(self):
         if self.programDir:
             path = os.path.join(self.programDir, self.name)
@@ -86,7 +88,7 @@ class Builder:
             return path
         else:
             return self.name
-        
+
     def clean(self, dir=None, projectFile=None):
         """
         dir = the directory containing the project file
@@ -95,38 +97,37 @@ class Builder:
         if self.isAvailable():
             result = runInDir("%s clean" % self.getProgramPath(), dir)
             return result
-            
+
         return False
-        
+
     def configure(self, options=None):
         # if we don't have configure, just report success
         return True
-            
+
     def build(self, dir=None, projectFile=None, targets=None, options=None):
         if self.isAvailable():
             if options:
-                print "Options %r" % string.join(options, " ")
                 optionList = list(options)
             else:
-            	optionList = []
+                optionList = []
 
             optionList.insert(0, self.getProgramPath())
-            
+
             result = runInDir(optionList, dir)
 
             return result
-        
+
         return 1
 
     def install(self, dir=None, options=None):
         if self.isAvailable():
-            
+
             args = ["make", "install"]
             if options:
-            	args.extend(options)
+                args.extend(options)
             result = runInDir(args, dir)
             return result
-            
+
         return 1
 
 # Concrete subclasses of abstract Builder interface
@@ -134,25 +135,25 @@ class Builder:
 class GNUMakeBuilder(Builder):
     def __init__(self, commandName="make", formatName="GNUMake"):
         Builder.__init__(self, commandName=commandName, formatName=formatName)
-        
+
 
 class XCodeBuilder(Builder):
     def __init__(self, commandName="xcodebuild", formatName="XCode"):
         Builder.__init__(self, commandName=commandName, formatName=formatName)
 
-        
+
 class AutoconfBuilder(GNUMakeBuilder):
     def __init__(self, formatName="autoconf"):
         GNUMakeBuilder.__init__(self, formatName=formatName)
-        
+
     def configure(self, dir=None, options=None):
         #olddir = os.getcwd()
         #os.chdir(dir)
-        
+
         configdir = dir
         if not dir:
             configdir = os.getcwd()
-        
+
         configure_cmd = ""
         while os.path.exists(configdir):
             config_cmd = os.path.join(configdir, "configure")
@@ -160,36 +161,41 @@ class AutoconfBuilder(GNUMakeBuilder):
                 parentdir = os.path.abspath(os.path.join(configdir, ".."))
                 if configdir == parentdir:
                     break
-                
+
                 configdir = parentdir 
             else:
                 configure_cmd = config_cmd
                 break
-                
+
         if not configure_cmd:
             sys.stderr.write("Could not find configure script at %r. Have you run autoconf?\n" % dir)
             return 1
-        
+
         optionsStr = string.join(options, " ") if options else ""
         result = os.system("%s %s" % (configure_cmd, optionsStr))
         #os.chdir(olddir)
         return result
-        
+
 
 class MSVCBuilder(Builder):
     def __init__(self):
-        Builder.__init__(self, commandName="nmake", formatName="msvc")
-        
-    def isAvailable(self):
-        return os.system("%s /help" % self.name) == 0
+        Builder.__init__(self, commandName="nmake.exe", formatName="msvc")
 
+    def isAvailable(self):
+        PATH = os.environ['PATH'].split(os.path.pathsep)
+        for p in PATH:
+            if os.path.exists(os.path.join(p, self.name)):
+                return True
+        return False
+
+        
 class MSVCProjectBuilder(Builder):
     def __init__(self):
         Builder.__init__(self, commandName="VCExpress.exe", formatName="msvcProject")
         for key in ["VS90COMNTOOLS", "VC80COMNTOOLS", "VC71COMNTOOLS"]:
             if os.environ.has_key(key):
                 self.prgoramDir = os.path.join(os.environ[key], "..", "IDE")
-                
+
         if self.programDir == None:
             for version in ["9.0", "8", ".NET 2003"]:
                 msvcDir = "C:\\Program Files\\Microsoft Visual Studio %s\\Common7\\IDE" % version
@@ -208,7 +214,7 @@ class MSVCProjectBuilder(Builder):
                 if os.path.exists(path):
                     self.name = "devenv.com"
                     return True
-        
+
         return False
 
 builders = [GNUMakeBuilder, XCodeBuilder, AutoconfBuilder, MSVCBuilder, MSVCProjectBuilder]
@@ -219,5 +225,5 @@ def getAvailableBuilders():
         thisBuilder = symbol()
         if thisBuilder.isAvailable():
             availableBuilders[thisBuilder.formatName] = symbol
-                
+
     return availableBuilders
